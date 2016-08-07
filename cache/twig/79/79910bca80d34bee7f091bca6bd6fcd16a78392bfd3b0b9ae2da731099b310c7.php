bc63af5dd4316019398f07268e1861bf6e4fed90
Michael Fangman <fangmanm@gmail.com>
2016-08-07 15:49:28 -0500

    adsf

---
+++ cache/twig/79/79910bca80d34bee7f091bca6bd6fcd16a78392bfd3b0b9ae2da731099b310c7.php
@@ -0,0 +1,29 @@
+<?php
+
+/* forms/fields/ignore/ignore.html.twig */
+class __TwigTemplate_e7d2459232833df81b3ed6f3f906498c4acd92076bf9465906e6933bdc131a9d extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+    }
+
+    public function getTemplateName()
+    {
+        return "forms/fields/ignore/ignore.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array ();
+    }
+}
