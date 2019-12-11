5f464c07a035bec863b6b706c2940ea786f5b7da
Michael Fangman <hello@michaelfangman.com>
2019-12-10 22:31:28 -0600

    potential fix to email plugin

---
+++ cache/compiled/files/f8845271f74531f484008eae47226aca.yaml.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/Applications/MAMP/htdocs/portfolio-grav/user/plugins/admin/languages/tlh.yaml',
+    'modified' => 1576035497,
+    'data' => [
+        'PLUGIN_ADMIN' => [
+            'LOGIN_BTN_FORGOT' => 'lIj',
+            'BACK' => 'chap',
+            'NORMAL' => 'motlh',
+            'YES' => 'HIja\'',
+            'NO' => 'Qo\'',
+            'DISABLED' => 'Qotlh'
+        ]
+    ]
+];
